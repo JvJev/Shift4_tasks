f6d6d115f49e85e6b4d8c0380c04e9135519c9d9
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2023-11-04 21:59:36 +0200

    starting-new-quickLaunchPage

---
+++ 1/first_ui/src/pages/quickLaunch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QuickLaunch() {
+  return (
+    <div>
+      <h1>Quick Launch Page here. More content later</h1>
+    </div>
+  );
+}
+
+export default QuickLaunch;
